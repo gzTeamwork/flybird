569e4a434b4ce980cf26b1e108b96b30ec2345ad
Zico <21520993@qqcom>
2019-04-25 00:25:48 +0800

    增加重力世界封装

--- src/game/lib/modules/GameWorld.ts
+++ src/game/lib/modules/GameWorld.ts
@@ -3,21 +3,32 @@ module GameWorld {
 
 		public Game;
 
+		public constructor(Game) {
+			this.Game = Game;
+		}
+
 		public Layers: { [key: string]: number } = {
-			 'Bottom': 0, 'Main': 1, 'Effect': 2, 'Ui': 3, 'Pad': 4 
-			}
+			'Bottom': 0, 'Main': 1, 'Effect': 2, 'Ui': 3, 'Pad': 4
+		}
+		
 
 		//	获取图层组编号
 		public getLayersIndex = (name: string): number | null => {
 			return this.Layers[name] || null;
 		}
 
-		public constructor(Game) {
-			this.Game = Game;
+		//	获取图层
+		public getLayer = (name:string): GameLayer.LayerGroup=>{
+			return this.Game.getChildAt(this.getLayersIndex(name))||null;
+		}
+
+		//	插入元素到图层
+		public addChildTo(ele:egret.Sprite,layersName:string){
+			this.getLayer(layersName).addChild(ele);
 		}
 
 		//	创建图层		
-		public createLayers(Layers: { [key: string]: number }){
+		public createLayers(Layers: { [key: string]: number }) {
 
 			this.Layers = Object.assign(this.Layers, Layers)
 			console.log("需要创建的图层", this.Layers)
@@ -39,8 +50,8 @@ module GameWorld {
 		public loadScene = (sceneName: string, layerName: string) => {
 			let _scene = new GameScene.basicScene(sceneName);
 			let _layer = this.Game.getChildAt(this.getLayersIndex(layerName));
-			_scene.width = this.Game.stageWidth,_scene.height = this.Game.stageHeight;
-			_layer.addChildAt(_scene,0);
+			_scene.width = this.Game.stageWidth, _scene.height = this.Game.stageHeight;
+			_layer.addChildAt(_scene, 0);
 		}
 	}
 }
